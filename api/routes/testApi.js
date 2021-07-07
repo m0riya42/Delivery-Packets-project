c7f3b16cef2096ffdfc6ae5dc4e7e36af582be2f
shirb113 <bracya@g.jct.ac.il>
2021-07-07 16:37:55 +0300

    view

--- api/routes/testApi.js
+++ api/routes/testApi.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var router = express.Router();
 router.get("/", function (req, res, next) {
-    res.send("API is working properly");
+    // res.send("API is NOT working properly");
+    res.render('index', { });
+
 });
 module.exports = router;
