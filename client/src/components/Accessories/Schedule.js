fda5fc846c8b02d560e962258760b05c9d5aa40d
shirb113 <bracya@g.jct.ac.il>
2021-08-03 09:56:49 +0300

    temp

--- client/src/components/Accessories/Schedule.js
+++ client/src/components/Accessories/Schedule.js
@@ -1,13 +1,4 @@
 import React, { useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
-import { Checkbox } from '@material-ui/core';
 import Grid from '@material-ui/core/Grid';
 import 'date-fns';
 import DateFnsUtils from '@date-io/date-fns';
@@ -18,38 +9,9 @@ import { Container } from '@material-ui/core';
 import MapCard from './MapCard';
 
 
-// var users = []
-// axios.post('http://localhost:9000/usersInfo/getUsers')
-//     .then(res => {
-//         //console.log(res.data);
-//         users = res.data;
-//         //console.log(users);
-//     })
-//     .catch(err => {
-//         console.log(err);
-//     })
-
-const useStyles = makeStyles({
-    table: {
-        minWidth: 400,
-    },
-});
-
 
 const Schedule = ({handlers}) => {
-     //const classes = useStyles();
-
-
-
-    // function createData(id, fullName, address) {
-    //     return { id, fullName, address };
-    // }
-
-    // const rows = [];
-
-    // users.map((user) => {
-    //     rows.push(createData(user.id, user.fullName, user.address))
-    // })
+    
 
     const [selectedDate, setSelectedDate] = React.useState(new Date());
     const handleDateChange = (date) => {
@@ -63,53 +25,6 @@ const Schedule = ({handlers}) => {
         { title: "ירושלים", image: "/assets/images/map.jpg" },
     ];
     return (
-
-        //     <div>
-        //         <MuiPickersUtilsProvider utils={DateFnsUtils} locale={deLocale}>
-        //             <Grid container justifyContent="space-around" >
-        //                 <KeyboardDatePicker 
-        //                     disableToolbar
-        //                     variant="inline"
-        //                     format="dd/MM/yyyy"
-        //                     margin="normal"
-        //                     id="date-picker-inline"
-        //                     //label="Date picker inline"
-        //                     value={selectedDate}
-        //                     onChange={handleDateChange}
-        //                     KeyboardButtonProps={{ 'aria-label': 'change date' }}
-        //                 />
-        //             </Grid>
-        //         </MuiPickersUtilsProvider>
-
-        //         <br></br><br></br>
-        //         <TableContainer component={Paper} style={{ margin: "auto", width: "80%" }}>
-        //             <Table className={classes.table} size="small" aria-label="a dense table">
-        //                 <TableHead>
-        //                     <TableRow style={{ background: "#3bb6b1" }}>
-        //                         <TableCell style={{ fontWeight: "bold", color: "white", fontSize: "15px" }} align="right"></TableCell>
-        //                         <TableCell style={{ fontWeight: "bold", color: "white", fontSize: "15px" }} align="right">כתובת</TableCell>
-        //                         <TableCell style={{ fontWeight: "bold", color: "white", fontSize: "15px" }} align="right">שם מלא</TableCell>
-        //                         <TableCell style={{ fontWeight: "bold", color: "white", fontSize: "15px" }} align="right">תעודת זהות</TableCell>
-        //                     </TableRow>
-        //                 </TableHead>
-        //                 <TableBody>
-        //                     {rows.map((row) => (
-        //                         <TableRow key={row.name} style={{ height: "50px" }}>
-        //                             <TableCell align="right" padding="checkbox">
-        //                                 <Checkbox />
-        //                             </TableCell>
-        //                             <TableCell style={{ fontSize: "15px" }} align="right">{row.address}</TableCell>
-        //                             <TableCell style={{ fontSize: "15px" }} align="right">{row.fullName}</TableCell>
-        //                             <TableCell style={{ fontSize: "15px" }} align="right">{row.id}</TableCell>
-        //                         </TableRow>
-        //                     ))}
-        //                 </TableBody>
-        //             </Table>
-        //         </TableContainer>
-        //         <div style={{ background: "transparent" }}>
-        //             <input type="button" className="btn btn-primary px-4" value="שמור לוז עבודה" style={{ background: "#3bb6b1", fontWeight: 'bold', marginLeft: "196px", marginTop: "27px" }} />
-        //         </div>
-        //     </div >
 
         <div>
             <MuiPickersUtilsProvider utils={DateFnsUtils} locale={deLocale}>
