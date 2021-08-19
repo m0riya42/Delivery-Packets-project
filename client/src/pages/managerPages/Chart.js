608f527934132db46d2707acd9e8db711efcaeab
שיר ברכיה <bracya@g.jct.ac.il>
2021-08-19 15:51:59 +0300

    chart bar + pie

--- client/src/pages/managerPages/Chart.js
+++ client/src/pages/managerPages/Chart.js
@@ -3,9 +3,17 @@ import { makeStyles } from "@material-ui/core/styles";
 import DefaultPieChart from '../../components/Accessories/Charts/DefaultPieChart';
 import PagesTitles from "../../components/Accessories/Charts/PagesTitles";
 import ViewDayIcon from '@material-ui/icons/ViewDay';
+import PieChartIcon from '@material-ui/icons/PieChart';
+import BarChartIcon from '@material-ui/icons/BarChart';
 import ViewWeekIcon from '@material-ui/icons/ViewWeek';
 import DateRangeIcon from '@material-ui/icons/DateRange';
 import TabPanel from "../../components/Accessories/Charts/TabPanel";
+import DayChart from "./PieCharts/DayChart";
+import WeekChart from "./PieCharts/WeekChart";
+import MonthChart from "./PieCharts/MonthChart";
+import DayBarChart from "./PieCharts/DayBarChart";
+import WeekBarChart from "./PieCharts/WeekBarChart";
+import MonthBarChart from "./PieCharts/MonthBarChart";
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -15,41 +23,54 @@ const useStyles = makeStyles((theme) => ({
     }
 }));
 
-export default function Chart() {
+const Chart = ({}) => {
+
     const classes = useStyles();
     const [value, setValue] = useState(0);
 
     const listOfTabs = [
-        { label: "חתך יומי", icon: <ViewDayIcon /> },
-        { label: "חתך שבועי", icon: <ViewWeekIcon /> },
-        { label: "חתך חודשי", icon: <DateRangeIcon /> },
+        { label: "חתך יומי", icon: <PieChartIcon /> },
+        { label: " חתך יומי", icon: <BarChartIcon /> },
+        { label: "חתך שבועי", icon: <PieChartIcon /> },
+        { label: "חתך שבועי", icon: <BarChartIcon /> },
+        { label: "חתך חודשי", icon: <PieChartIcon /> },
+        { label: "חתך חודשי", icon: <BarChartIcon /> },
     ]
 
-    return (
-        <div className={classes.root} style={{ direction: "rtl" }} >
+    return ( 
+  
+        <div className={classes.root} style={{ direction: "rtl", background: "transparent" }} >
             <PagesTitles value={value} setValue={setValue} listOfTabs={listOfTabs} />
 
             {/* Pages Itself */}
             <TabPanel value={value} index={0}>
-                <DefaultPieChart data={[
-                    { title: 'One', value: 55, color: '#E38627' },
-                    { title: 'Two', value: 15, color: '#C13C37' },
-                    { title: 'Three', value: 20, color: '#6A2135' },
-                ]} />
-            </TabPanel>
-
-            <TabPanel value={value} index={1}>
-                Page Two
+                <DayChart/>
             </TabPanel>
 
             <TabPanel value={value} index={2}>
-                Page Three
+               <WeekChart />
+            </TabPanel>
+
+            <TabPanel value={value} index={4}>
+                <MonthChart />
+            </TabPanel>
+
+            <TabPanel value={value} index={1}>
+                <DayBarChart/>
+            </TabPanel>
+
+            <TabPanel value={value} index={3}>
+               <WeekBarChart />
+            </TabPanel>
+
+            <TabPanel value={value} index={5}>
+                <MonthBarChart />
             </TabPanel>
         </div>
     );
 
 }
-
+export default Chart;
 
 
 
